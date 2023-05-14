a279c7a0eaba917050a96176a50c59105d429c0b
manyids2 <arun.mukundan@gmail.com>
2023-05-14 12:31:14 +0200

    Added config from h-m-m, probably need to move to insert/move instead of create

--- lua/hmm/app.lua
+++ lua/hmm/app.lua
@@ -2,12 +2,37 @@ local a = vim.api
 local t = require("hmm.tree")
 local M = {}
 
-function M.setup()
+M.default_config = {
+	max_parent_node_width = 25,
+	max_leaf_node_width = 55,
+	line_spacing = 1,
+	align_levels = 0,
+	initial_depth = 1,
+	center_lock = false,
+	focus_lock = false,
+	max_undo_steps = 24,
+	clipboard = "os",
+	clipboard_file = "/tmp/h-m-m",
+	clipboard_in_command = "",
+	clipboard_out_command = "",
+	post_export_command = "",
+	symbol1 = "✓",
+	symbol2 = "✗",
+}
+
+function M.setup(config)
 	-- return if not hmm file
 	local filetype = a.nvim_exec2("echo expand('%:e')", { output = true }).output
 	if filetype ~= "hmm" then
 		return
 	end
+
+	-- get config
+	if config == nil then
+		config = M.default_config
+	end
+	M.config = vim.tbl_extend("keep", config, M.default_config)
+	P(M.config)
 
 	-- need to reopen, else nlines is 0
 	local filename = a.nvim_exec2("echo expand('%')", { output = true }).output
